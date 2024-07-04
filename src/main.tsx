38cca4534174896ae4f682e83d7fcbd98346559f
junnun15 <junnunprodhan@gmail.com>
2024-07-04 19:59:08 +0600

    done task

--- src/main.tsx
+++ src/main.tsx
@@ -1,40 +1,19 @@
 
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import App from './App';
-// import { ThemeProvider, CssBaseline } from '@mui/material';
-// import theme from './theme/theme';
-
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <ThemeProvider theme={theme}>
-//       <CssBaseline />
-//       <App />
-//     </ThemeProvider>
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { ThemeProvider, CssBaseline } from '@mui/material';
 import theme from './theme/theme';
 
+
 ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <div>
-        <div >
-          <div>
-            <App />
-          </div>
-        </div>
-      </div>
+      <App />
     </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
+
 
